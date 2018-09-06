5934fa5a3028d3dfedb0ec48e865ff2f63248f8e
antaresvalle <antaresvll561@gmail.com>
2018-09-05 23:44:52 -0500

    design update

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
-  });
        
+  window.onscroll = function() {
+    var bg = document.getElementById('bg');
+    var nav = this.document.getElementById('sideNavigation');
+    var about = document.getElementById('about');
+    var aboutS = document.getElementById('about-section'); 
+    var intro = document.getElementById('header-intro');
+    var constellation = this.document.getElementById('constellation');
+    if ( window.pageYOffset > 700 ) {
+      // bg.classList.add("bg-color");
+      constellation.classList.add("opacity");
+      aboutS.classList.add("index");
+      intro.classList.add("intro");
+      
+
+    } else {
+      bg.classList.remove("bg-color");
+      
+      intro.classList.remove("intro");
+      constellation.classList.remove("opacity");
+    }
+
+    if (window.pageYOffset > 300) {
+      nav.classList.add("show");
+      about.classList.add("fill-hexagon");
+      
+    } else {
+      nav.classList.remove("show");
+      about.classList.remove("fill-hexagon");
+      
+    }
+}
+function nombre (work){
+ work
+}
+
+nombre(work);
